6e1ed2a1c60d32181325a8e639a7f3db33035c80
Brandon Hill <bhill010@gmail.com>
2017-11-24 22:11:02 -0800

    Fix question generation consistency

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,9 @@ export const FETCH_QUESTIONS = 'fetch_questions';
 export const FETCH_QUESTION = 'fetch_question';
 
 const ROOT_URL = `https://qriusity.com/v1/questions`;
+const PAGE_NUMBER = Math.floor(Math.random() * 2000);
 
 export function fetchQuestions(){
-  let PAGE_NUMBER = Math.floor(Math.random() * 2000);
   const request = axios.get(`${ROOT_URL}?page=${PAGE_NUMBER}&limit=5`);
   // console.log("action request", request);
 
